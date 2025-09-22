338db4d34b463c5cad34035619b998a2d7c4ec12
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-09-22 16:22:45 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// login button functionality
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  //   mobile number value
+  const mobileNumber = 123456789;
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  //   pin numebr value
+  const pinNumber = 1234;
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  // comparing function
+ if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalied Credentials");
+  }
+});
